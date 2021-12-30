a84a65cd1078d824725620fca6f244e2b5847fa4
Fernanda <fernandaneeri@gmail.com>
2021-12-30 12:55:57 -0300

    adicionando o segundo exercicio de POO

---
+++ exercicio_proposto_2_poo/src/program/Program.java
@@ -0,0 +1,38 @@
+package program;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import employee.Employee;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		Employee emp = new Employee();
+
+		System.out.print("Name: ");
+		emp.name = sc.next();
+
+		System.out.print("Gross salary: ");
+		emp.salary = sc.nextDouble();
+
+		System.out.print("Tax: ");
+		emp.tax = sc.nextDouble();
+
+		System.out.println(emp.toString());
+		
+		System.out.print("\nWhich percentage to increase salary? ");
+		double percentage = sc.nextDouble();
+		emp.increaseSalary(percentage); 
+		
+		
+		System.out.println(emp.toString());
+		
+
+		sc.close();
+	}
+
+}
